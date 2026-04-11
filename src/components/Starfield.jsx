aa781aef75c00a31ba235dd6bd6f4e1500eabac5
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2026-04-10 20:25:43 -0500

    Create Starfield.jsx

---
+++ src/components/Starfield.jsx
@@ -0,0 +1,11 @@
+export default function Starfield() {
+  return (
+    <>
+      <div className="stars stars-a" />
+      <div className="stars stars-b" />
+      <div className="particles" />
+      <div className="glow-orb orb-1" />
+      <div className="glow-orb orb-2" />
+    </>
+  );
+}
